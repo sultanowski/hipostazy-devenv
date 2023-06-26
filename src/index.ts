64721172ce525bdcc11207d97adf77584954d40b
sultanowski <sultanowski.d@gmail.com>
2023-06-26 17:26:10 +0200

    feat: barba.js && three - interactions and animations.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,57 @@
-import { greetUser } from '$utils/greet';
+import barba from '@barba/core';
+import gsap from 'gsap';
+import hoverEffect from 'hover-effect';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  barba.init({
+    transitions: [
+      {
+        name: 'blog',
+        async leave(data) {
+          await gsap.to(data.current.container, {
+            opacity: 0,
+            duration: 0.5,
+          });
+        },
+        beforeEnter() {
+          generateCanvas();
+        },
+        enter(data) {
+          gsap.from(data.next.container, {
+            opacity: 1,
+            duration: 0.5,
+          });
+        },
+      },
+    ],
+  });
+
+  function getRandomArbitrary(min: number, max: number) {
+    return Math.random() * (max - min) + min;
+  }
+
+  function generateCanvas() {
+    const firstImage = document.querySelectorAll(
+      '.blogpost-image_thumbnail'
+    ) as NodeListOf<HTMLImageElement>;
+    const secondImage = document.querySelectorAll(
+      '.blogpost-image_image'
+    ) as NodeListOf<HTMLImageElement>;
+    const blogposts = document.querySelectorAll(
+      '[zu-hipostazy="blogpost-morph"]'
+    ) as NodeListOf<HTMLElement>;
+    for (let i = 0; i < blogposts.length; i++) {
+      new hoverEffect({
+        parent: blogposts[i],
+        intensity: 0.25,
+        angle: getRandomArbitrary(1, 4),
+        image1: `${firstImage[i].src}`,
+        image2: `${secondImage[i].src}`,
+        displacementImage:
+          'https://uploads-ssl.webflow.com/644bc713fd500afe82fdab78/6499521f8931839715de7ed0_displacement-mask.png',
+      });
+    }
+  }
+  generateCanvas();
 });
